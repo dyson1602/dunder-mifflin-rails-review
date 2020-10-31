5c8fb484b1f9e6c955dd24f4b50e89713134db2d
dyson1602 <cclark000@citymail.cuny.edu>
2020-10-31 16:15:22 -0400

    all works

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,14 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+        render :index
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+        @employees = Employee.all.select{|employee| employee.dog == @dog}
+  
+        render :show
+    end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,50 @@
 class EmployeesController < ApplicationController
+
+    def index
+        @employees = Employee.all
+        render :index
+    end
+
+    def show
+        @employee = Employee.find(params[:id])
+        render :show
+    end
+
+    def new
+        @employee = Employee.new
+        render :new
+    end
+
+    def create
+        @employee = Employee.create(employee_params)
+        
+        if @employee.valid?
+            redirect_to employee_path(@employee)
+        else flash[:employee_errors] = @employee.errors.full_messages
+            redirect_to new_employee_path
+        end
+    end
+
+    def edit
+        @employee = Employee.find(params[:id])
+        render :edit
+    end
+
+
+    def update
+        @employee = Employee.update(employee_params)
+
+        if @employee.valid?
+            redirect_to employee_path(@employee)
+        else flash[:employee_errors] = @employee.errors.full_messages
+            redirect_to edit_employee_path
+        end
+    end
+
+    private
+
+    def employee_params
+        params.require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :dog_id, :img_url)
+    end
+
 end
